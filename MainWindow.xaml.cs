f1c02af1db67b7ced9188584c8a4047b7fad8a15
Dfying <demian@ati2000.co.kr>
2022-06-08 00:41:24 +0900

    Serial open 불가.. 해결필요

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Ports;
 
 namespace Valuesaver
 {
@@ -20,9 +21,56 @@ namespace Valuesaver
     /// </summary>
     public partial class MainWindow : Window
     {
+        public SerialPort _port;
+                
         public MainWindow()
         {
             InitializeComponent();
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                port.Items.Add(s);
+            }
+        }
+
+        private void connect_Click(object sender, RoutedEventArgs e)
+        {
+            if (_port.IsOpen)
+            {
+                _port.PortName = port.Name;
+                _port.BaudRate = 9600;
+                _port.DataBits = 8;
+                _port.StopBits = StopBits.One;
+                _port.Parity = Parity.None;
+                _port.DataReceived += new SerialDataReceivedEventHandler(_portDataReceived);
+
+                _port.Open();
+                label.Content = "연결됨";
+                port.IsEnabled = false;
+            }
+            else
+            {
+                label.Content = "이미 연결됨";
+            }
+        }
+
+        private void disconnect_Click(object sender, RoutedEventArgs e)
+        {
+            if(_port.IsOpen)
+            {
+                _port.Close();
+                port.IsEnabled = true;
+
+                label.Content = "연결종료";
+            }
+            else
+            {
+                label.Content = "이미 연결종료";
+            }
+        }
+
+        private void _portDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
         }
     }
 }
