d348d139ffb4f4f9a080ca808382e8fad57479f3
KJ <Dfying@gmail.com>
2022-06-09 17:33:35 +0900

    Open 불가 이슈 해결, 데이터 송수신 작업 시작

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO.Ports;
+using System.Threading;
 
 namespace Valuesaver
 {
@@ -21,56 +22,87 @@ namespace Valuesaver
     /// </summary>
     public partial class MainWindow : Window
     {
-        public SerialPort _port;
-                
+        public SerialPort _port = new SerialPort();
+        public const string ETX = "\r\n";
+        public const char STX = (char)0x02;
+        public const char CR = (char)0x0D;
+        public const char LF = (char)0x0A;
+        public const string inStream = "inStream";
+
+        public const string MA = "MA";
+
+        string receivedData;
+
         public MainWindow()
         {
             InitializeComponent();
-            foreach (string s in SerialPort.GetPortNames())
+            string[] Portno = SerialPort.GetPortNames();
+            foreach (string portnum in Portno)
             {
-                port.Items.Add(s);
+                port.Items.Add(portnum);
             }
         }
 
         private void connect_Click(object sender, RoutedEventArgs e)
         {
-            if (_port.IsOpen)
+
+            if (!_port.IsOpen)
             {
-                _port.PortName = port.Name;
+                if (port.SelectedItem == null)
+                {
+                    label.Text = "선택 가능한 PORT 없음";
+                    return;
+                }
+
+                _port.PortName = port.SelectedItem.ToString();
                 _port.BaudRate = 9600;
                 _port.DataBits = 8;
                 _port.StopBits = StopBits.One;
                 _port.Parity = Parity.None;
                 _port.DataReceived += new SerialDataReceivedEventHandler(_portDataReceived);
-
                 _port.Open();
-                label.Content = "연결됨";
+
                 port.IsEnabled = false;
+                label.Text = "연결됨";
+                receivedata.Text = "Start";
             }
             else
             {
-                label.Content = "이미 연결됨";
+                label.Text = "이미 연결됨";
             }
+
         }
 
         private void disconnect_Click(object sender, RoutedEventArgs e)
         {
-            if(_port.IsOpen)
+            if (_port.IsOpen == true)
             {
                 _port.Close();
                 port.IsEnabled = true;
-
-                label.Content = "연결종료";
+                label.Text = "연결종료";
             }
             else
             {
-                label.Content = "이미 연결종료";
+                label.Text = "이미 연결종료";
             }
         }
 
-        private void _portDataReceived(object sender, SerialDataReceivedEventArgs e)
+        private void datasaver_Click(object sender, RoutedEventArgs e)
         {
-
+            if (_port.IsOpen)
+            {
+                _port.Write(MA);
+            }
+            else
+            {
+                return;
+            }
+        }
+        
+        private void _portDataReceived(object sender, EventArgs e)
+        {
+            receivedData = _port.ReadExisting();
+            receivedata.Text += string.Format("{0:X2}", receivedData);
         }
     }
 }
